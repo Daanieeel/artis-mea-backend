6c575d3f495ebd4c2cd3d5995f8009d54dd813b6
Daanieeel <d4niel.dd060306@gmail.com>
2024-03-02 01:33:49 +0100

    Initial commit

---
+++ dbHelpers.js
@@ -0,0 +1,25 @@
+// Database Helpers
+const getValidUnusedToken = async (token) => {
+  const [rows] = await connection.execute(
+    `SELECT bw.token_id
+    FROM bansystem_token
+    LEFT JOIN s3_whitelist.bansystem_whitelist bw on bansystem_token.token_id = bw.token_id
+    WHERE token = ?
+    AND bw.whitelist_id IS NULL
+    LIMIT 1;`, [token]);
+  return rows.length > 0 ? rows[0].token_id : null;
+};
+
+const isPlayerWhitelisted = async (name) => {
+  const [rows] = await connection.execute(
+    `SELECT 1
+    FROM bansystem_whitelist
+    WHERE bansystem_whitelist.player_id = ?
+    LIMIT 1;`, [name]);
+  return rows.length > 0;
+};
+
+const addPlayerToWhitelist = async (tokenId, name) => {
+  await connection.execute(
+    `INSERT INTO bansystem_whitelist (token_id, player_id) VALUE (?, ?);`, [tokenId, name]);
+};
---
+++ index.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const rateLimit = require('express-rate-limit');
+const mysql = require('mysql2/promise');
+
+const dbConfig = {
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'bansystem'
+};
+
+const app = express();
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100
+});
+
+app.use(limiter);
+app.use(express.json());
+
+const cors = require('cors');
+app.use(cors());
+
+app.patch('/redeem-token', async (req, res) => {
+  try {
+    const { name, token } = req.body;
+
+    console.debug('New request: ', req.body);
+
+    const requiredFields = ['name', 'token'];
+    const missingFields = validateFields(req.body, requiredFields);
+    if (missingFields.length > 0) {
+      res.status(400).json({
+        error: 'Missing required fields',
+        missingFields: missingFields
+      });
+      return;
+    }
+
+    const connection = await mysql.createConnection(dbConfig);
+
+    // Check if the token is valid and unused
+    const tokenId = await getValidUnusedToken(token);
+    if (!tokenId) {
+      res.status(400).json({ error: 'Invalid or used token' });
+      return;
+    }
+
+    // Check if the player is already whitelisted
+    const isWhitelisted = await isPlayerWhitelisted(name);
+    if (isWhitelisted) {
+      res.status(400).json({ error: 'Player is already whitelisted' });
+      return;
+    }
+
+    // Add the player to the whitelist
+    await addPlayerToWhitelist(tokenId, name);
+
+    res.send({
+      success: true,
+      token: token
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while processing your request.' });
+  }
+});
+
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+function validateFields(body, requiredFields) {
+  const missingFields = [];
+  for (let field of requiredFields) {
+    if (!body || !body[field]) {
+      missingFields.push(field);
+    }
+  }
+  return missingFields;
+}
